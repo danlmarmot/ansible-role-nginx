16a27cba3d0eb8a90241e537bcf115bab761c22f
dmckean <danlmarmot@gmail.com>
2016-03-25 14:11:36 -0700

    nginx role working.  Tagging as v1

---
+++ examples/install_role_requirements.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# -----
+# nginx has no role requirements, so this file is here simply for completeness
+# -----
+
+# ansible-galaxy install -r role_requirements.yml -p role_dependencies --force
